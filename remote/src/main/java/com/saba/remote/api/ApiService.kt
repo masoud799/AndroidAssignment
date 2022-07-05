72f1ab1e3aa3fc07738b07d3eb35e8e51507e47b
mokhtari <m.mokhtari@gmail.com>
2022-07-05 22:02:18 +0430

    We created ApiService.kt class that handles all endpoint processes in remote module.

---
+++ remote/src/main/java/com/saba/remote/api/ApiService.kt
@@ -0,0 +1,18 @@
+package com.saba.remote.api
+
+import com.saba.remote.model.SearchNetworkModel
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+/**
+ * The main services that handles all endpoint processes
+ */
+interface ApiService {
+
+    /**
+     * Get search result
+     */
+    @GET("movie/movie/list/tagid/1000300/text/{Query}/sug/on")
+    suspend fun search(@Path("Query") query : String) : SearchNetworkModel
+
+}
